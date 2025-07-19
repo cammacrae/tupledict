ad80fa10f8d261c6b763828f7ef00066c98eddc1
Cam MacRae <cam.macrae@gmail.com>
2025-07-19 15:04:22 +1000

    feat: add typing

--- tupledict/dictlist.py
+++ tupledict/dictlist.py
@@ -1,19 +1,23 @@
 from itertools import chain
+from typing import Dict, Generic, Iterable, List, Tuple, TypeVar
+
+K = TypeVar("K")
+V = TypeVar("V")
 
 
-class DictList:
+class DictList(Generic[K, V]):
     """
     Inspired by Anton Dries https://pypi.org/project/tupledict/
     """
 
-    def __init__(self, indx):
-        self._indx = indx
+    def __init__(self, indx: Iterable[Dict[K, V]]) -> None:
+        self._indx: Iterable[Dict[K, V]] = indx
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: K) -> List[V]:
         return [d[key] for d in self._indx if key in d]
 
-    def values(self):
+    def values(self) -> List[V]:
         return list(chain(*(d.values() for d in self._indx)))
 
-    def items(self):
+    def items(self) -> List[Tuple[K, V]]:
         return list(chain(*(d.items() for d in self._indx)))
--- tupledict/tupledict.py
+++ tupledict/tupledict.py
@@ -1,17 +1,37 @@
-from collections.abc import Iterable
+from collections.abc import Iterable as IterableABC
 from types import GeneratorType
+from typing import (
+    Any,
+    Dict,
+    Generic,
+    Hashable,
+    Optional,
+    Tuple,
+    TypeVar,
+    Union,
+)
 
 from .dictlist import DictList
 
+K = TypeVar("K", bound=Hashable)
+V = TypeVar("V")
 
-class TupleDict(dict):
+
+class TupleDict(dict[Tuple[K, ...], V], Generic[K, V]):
     """
     Somewhat inspired by Anton Dries https://pypi.org/project/tupledict/
     """
 
-    def __init__(self, *args, **kwargs):
+    _key_len: Optional[int]
+    _key_indx: Dict[Any, Any]
+
+    def __init__(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
         self._key_len = None
-        self._key_indx = {}
+        self._key_indx: Dict[Any, Any] = {}
         super().__init__()
         if args:
             if len(args) > 1:
@@ -19,39 +39,37 @@ class TupleDict(dict):
             arg = args[0]
             if isinstance(arg, dict):
                 self.update({self._valid_key(k): v for k, v in arg.items()})
-            elif isinstance(arg, Iterable):
+            elif isinstance(arg, IterableABC):
                 self.update(((self._valid_key(k), v) for k, v in arg))
         if kwargs:
             self.update({self._valid_key(k): v for k, v in kwargs.items()})
         for k, v in self.items():
             self._add_indx_key(k, v)
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: Union[K, Tuple[K, ...]], value: V) -> None:
         key = self._valid_key(key)
         super().__setitem__(key, value)
         self._add_indx_key(key, value)
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: Union[K, Tuple[K, ...]]) -> V:
         key = self._valid_key(key)
         return super().__getitem__(key)
 
-    def select(self, *keys):
+    def select(self, *keys: Any) -> Any:
         key = tuple(keys)
         key = self._valid_key(key)
         if any(
             v == "*" or isinstance(v, list) or isinstance(v, GeneratorType) for v in key
         ):
-            tmp = self._key_indx
+            tmp: Any = self._key_indx
             for k in key[:-1]:
                 if k == "*":
-                    # wildcard
                     tmp = DictList(tmp.values())
                 elif isinstance(k, list) or isinstance(k, GeneratorType):
                     tmp = DictList([v for kee, v in tmp.items() if kee in k])
                 else:
                     tmp = tmp[k]
             if key[-1] == "*":
-                # wildcard
                 return list(tmp.values())
             elif isinstance(key[-1], list) or isinstance(key[-1], GeneratorType):
                 return [v for k, v in tmp.items() if k in key[-1]]
@@ -60,13 +78,13 @@ class TupleDict(dict):
         else:
             return self.__getitem__(key)
 
-    def _valid_key(self, key):
+    def _valid_key(self, key: Union[K, Tuple[K, ...]]) -> Tuple[K, ...]:
         if not isinstance(key, tuple):
             key = (key,)
         self._check_key_len(len(key))
         return key
 
-    def _check_key_len(self, key_len):
+    def _check_key_len(self, key_len: int) -> None:
         if self._key_len is None:
             self._key_len = key_len
         elif self._key_len != key_len:
@@ -74,14 +92,12 @@ class TupleDict(dict):
                 f"Key length is {key_len} but this TupleDict has key length {self._key_len}."
             )
 
-    def _add_indx_key(self, key, value):
+    def _add_indx_key(self, key: Tuple[K, ...], value: V) -> None:
         """
         Read the test.
         """
         tmp = self._key_indx
         for k in key[:-1]:
-            # worth trying this I think
-            # tmp[k] = tmp.get(k, {})
             if k not in tmp:
                 tmp[k] = {}
             tmp = tmp[k]
